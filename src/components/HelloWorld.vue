f48db9a8877a630633cfd7aae41ff54446d66a0b
Gustavo Costa <gucostarp@gmail.com>
2021-04-15 17:15:48 -0300

    Inicialização do layout

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+   <ion-card>
+    <ion-card-header>
+      <ion-card-subtitle>Confira!</ion-card-subtitle>
+      <ion-card-title>Prato do Dia</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      Arroz, Feijão, Purê de Batata, Contra-filé e Salada. 
+    </ion-card-content>
+  </ion-card>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-page main class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-toggle>
+            <ion-button>
+              <ion-icon slot="icon-only" name="menu"></ion-icon>
+            </ion-button>
+          </ion-menu-toggle>
+        </ion-buttons>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content padding>
+      <hello-world />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  created() {
+    console.log("onCreated");
+    // const menuCtrl = document.querySelector("ion-menu-controller");
+    // menuCtrl?.enable(true);
+  }
+};
+</script>
